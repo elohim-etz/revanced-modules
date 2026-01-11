b951cab8a2c5cfe974fb82c186cae6ac88da7a1b
Naveen <133593113+elohim-etz@users.noreply.github.com>
2026-01-11 11:26:41 +0530

    chore: update for morphe
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- build-termux.sh
+++ build-termux.sh
@@ -29,33 +29,33 @@ if [ ! -f ~/.rvmm_"$(date '+%Y%m')" ]; then
 	yes "" | pkg update -y && pkg upgrade -y && pkg install -y git curl jq openjdk-17 zip
 	: >~/.rvmm_"$(date '+%Y%m')"
 fi
-mkdir -p /sdcard/Download/revanced-magisk-module/
+mkdir -p /sdcard/Download/morphe-builder/
 
-if [ -d revanced-magisk-module ] || [ -f config.toml ]; then
-	if [ -d revanced-magisk-module ]; then cd revanced-magisk-module; fi
-	pr "Checking for revanced-magisk-module updates"
+if [ -d morphe-builder ] || [ -f config.toml ]; then
+	if [ -d morphe-builder ]; then cd morphe-builder; fi
+	pr "Checking for morphe-builder updates"
 	git fetch
 	if git status | grep -q 'is behind\|fatal'; then
-		pr "revanced-magisk-module is not synced with upstream."
-		pr "Cloning revanced-magisk-module. config.toml will be preserved."
+		pr "morphe-builder is not synced with upstream."
+		pr "Cloning morphe-builder. config.toml will be preserved."
 		cd ..
-		cp -f revanced-magisk-module/config.toml .
-		rm -rf revanced-magisk-module
-		git clone https://github.com/j-hc/revanced-magisk-module --recurse --depth 1
-		mv -f config.toml revanced-magisk-module/config.toml
-		cd revanced-magisk-module
+		cp -f morphe-builder/config.toml .
+		rm -rf morphe-builder
+		git clone https://github.com/elohim-etz/morphe-builder --recurse --depth 1
+		mv -f config.toml morphe-builder/config.toml
+		cd morphe-builder
 	fi
 else
-	pr "Cloning revanced-magisk-module."
-	git clone https://github.com/j-hc/revanced-magisk-module --depth 1
-	cd revanced-magisk-module
+	pr "Cloning morphe-builder."
+	git clone https://github.com/elohim-etz/morphe-builder --depth 1
+	cd morphe-builder
 	sed -i '/^enabled.*/d; /^\[.*\]/a enabled = false' config.toml
-	grep -q 'revanced-magisk-module' ~/.gitconfig 2>/dev/null ||
-		git config --global --add safe.directory ~/revanced-magisk-module
+	grep -q 'morphe-builder' ~/.gitconfig 2>/dev/null ||
+		git config --global --add safe.directory ~/morphe-builder
 fi
 
-[ -f ~/storage/downloads/revanced-magisk-module/config.toml ] ||
-	cp config.toml ~/storage/downloads/revanced-magisk-module/config.toml
+[ -f ~/storage/downloads/morphe-builder/config.toml ] ||
+	cp config.toml ~/storage/downloads/morphe-builder/config.toml
 
 if ask "Open rvmm-config-gen to generate a config?"; then
 	am start -a android.intent.action.VIEW -d https://j-hc.github.io/rvmm-config-gen/
@@ -63,11 +63,11 @@ fi
 printf "\n"
 until
 	if ask "Open 'config.toml' to configure builds?\nAll are disabled by default, you will need to enable at first time building"; then
-		am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-magisk-module/config.toml -t text/plain
+		am start -a android.intent.action.VIEW -d file:///sdcard/Download/morphe-builder/config.toml -t text/plain
 	fi
 	ask "Setup is done. Do you want to start building?"
 do :; done
-cp -f ~/storage/downloads/revanced-magisk-module/config.toml config.toml
+cp -f ~/storage/downloads/morphe-builder/config.toml config.toml
 
 ./build.sh
 
@@ -78,10 +78,10 @@ for op in *; do
 		pr "glob fail"
 		exit 1
 	}
-	mv -f "${PWD}/${op}" ~/storage/downloads/revanced-magisk-module/"${op}"
+	mv -f "${PWD}/${op}" ~/storage/downloads/morphe-builder/"${op}"
 done
 
-pr "Outputs are available in /sdcard/Download/revanced-magisk-module folder"
-am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-magisk-module -t resource/folder
+pr "Outputs are available in /sdcard/Download/morphe-builder folder"
+am start -a android.intent.action.VIEW -d file:///sdcard/Download/morphe-builder -t resource/folder
 sleep 2
-am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-magisk-module -t resource/folder
+am start -a android.intent.action.VIEW -d file:///sdcard/Download/morphe-builder -t resource/folder
